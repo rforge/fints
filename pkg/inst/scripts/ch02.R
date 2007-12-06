f17952b0b438a5c2f7a52d29afebfeae1785fba0
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-06 01:32:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fints@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/scripts/ch02.R
+++ pkg/inst/scripts/ch02.R
@@ -194,6 +194,7 @@ round(AIC5[,-1]/length(m.vw2697), 3)
 # for the Yule-Walker and MLE alternatives.
 # "ols" is very different from the others.
 
+# pp. 41-42 
 ##
 ## Figure 2.6.  Sample PACF of US Quarterly real GNP growth 
 ##
@@ -210,7 +211,18 @@ round(q.gnp4791.ar$aic, 3)
 q.gnp4791.ar$order
 
 data(m.vw2697) 
-ar.vw. <- ar(m.vw2697, order.max=3)
+(ar.vw. <- ar(m.vw2697, order.max=3))
+(ar.vw <- arima(m.vw2697, c(3,0,0)))
+
+mean(m.vw2697)
+# 0.009879116
+sum(coef(ar.vw)[1:3])
+# -0.02538
+# phi0 = mu*(1-sum(phi))
+mean(m.vw2697)*(1-sum(coef(ar.vw)[1:3]))
+# 0.01013
+
+
 names(ar.vw.)
 ar.vw.$ar # match bottom of p. 43
 ar.vw.$x.mean # vs. 0.0103 on p. 43
@@ -233,7 +245,7 @@ fit.2$Box.test
 
 
 # p. 49
-# Table 2.2
+# Table 2.2.  AR(3) Forecasts for Monthly Simple Returns of VW Index 
 methods(class="Arima")
 length(m.vw2697)
 # 864
@@ -243,43 +255,40 @@ ar.vw858 <- arima(m.vw2697[1:858], order=c(3,0,0))
 # actual
 m.vw2697[859:864]
 
-# Figure 2.7
-ul <- c(m.vw2697[858], pred.ar.vw$pred+1.96*pred.ar.vw$se)
-ll <- c(m.vw2697[858], pred.ar.vw$pred-1.96*pred.ar.vw$se)
+# Figure 2.7.  AR(5) Forecasts for Monthly Log Returns of VW Index
+(ar.vw858l <- arima(log(1+m.vw2697[1:858]), order=c(5,0,0)))
+sqrt(ar.vw858l$sigma2)
+# all the numbers match p. 50 except 'intercept'
+str(ar.vw858l)
+(1-sum(coef(ar.vw858l)[1:5]))*coef(ar.vw858l)["intercept"]
 
-plot(m.vw2697[851:864], type="b", ylim=range(ul, ll))
-lines(c(m.vw2697[858], pred.ar.vw$pred), type="b", col="red", lty="dashed")
-lines(ul, type="b", col="green", lty="dotted")
-lines(ll, type="b", col="green", lty="dotted")
+(pred.ar.vwl <- predict(ar.vw858l, 6))
 
-plot(m.vw2697[851:864], type="b", ylim=c(-.2, .2),
+
+ul <- c(log(1+m.vw2697[858]), with(pred.ar.vwl, pred+1.96*se))
+ll <- c(log(1+m.vw2697[858]), with(pred.ar.vwl, pred-1.96*se))
+
+plot(log(1+m.vw2697[851:864]),
+     type="b", ylim=range(ul, ll))
+lines(index(m.vw2697)[858:864], c(log(1+m.vw2697[858]), pred.ar.vwl$pred),
+      type="b", col="red", lty="dashed")
+lines(index(m.vw2697)[858:864], 
+      ul, type="b", col="green", lty="dotted")
+lines(index(m.vw2697)[858:864], 
+      ll, type="b", col="green", lty="dotted")
+
+plot(log(1+m.vw2697[851:864]), type="b", ylim=c(-.2, .2),
      xlab="year (and fraction)", ylab="vw")
-lines(c(m.vw2697[858], pred.ar.vw$pred), type="b", col="red", lty="dashed")
-lines(ul, type="b", col="green", lty="dotted")
-lines(ll, type="b", col="green", lty="dotted")
+lines(index(m.vw2697)[858:864], c(log(1+m.vw2697[858]), pred.ar.vwl$pred),
+      type="b", col="red", lty="dashed")
+lines(index(m.vw2697)[858:864], 
+      ul, type="b", col="green", lty="dotted")
+lines(index(m.vw2697)[858:864], 
+      ll, type="b", col="green", lty="dotted")
 
-# using an AR(5) per p. 50
-(ar5.vw858 <- arima(m.vw2697[1:858], order=c(5,0,0)))
-sqrt(ar5.vw858$sigma2)
-
-# Numbers are slightly diffrent from those on p. 50
-# Different estimation method?  
-
-(pred.ar5.vw <- predict(ar5.vw858, 6))
-
-# Figure 2.7
-ul5 <- c(m.vw2697[858], pred.ar5.vw$pred+1.96*pred.ar5.vw$se)
-ll5 <- c(m.vw2697[858], pred.ar5.vw$pred-1.96*pred.ar5.vw$se)
-
-plot(m.vw2697[851:864], type="b", ylim=c(-.2, .2),
-          xlab="year (and fraction)", ylab="vw")
-lines(c(m.vw2697[858], pred.ar5.vw$pred), type="b", col="red", lty="dashed")
-lines(ul5, type="b", col="green", lty="dotted")
-lines(ll5, type="b", col="green", lty="dotted")
-
-range(ul, ll)
-range(ul5, ll5)
-# The AR(3) and AR(5) forecasts are pretty similar.
+##
+## sec. 2.5.  Simple Moving Average Models
+##
 
 # p. 52-53
 # Figure 2.8
@@ -302,43 +311,238 @@ par(op)
              Box.test.lag=12))
 fit.ew$Box.test
 
+# pp. 55-56
+##
+## Table 2.3.  MA(9) Forecasts for EW
+##
+# data documented with ch01data, not ch02data 
+data(m.ibmvwewsp2603)
+str(m.ibmvwewsp2603)
+plot(m.ibmvwewsp2603[, "EW"])
+abline(v=index(m.ibmvwewsp2603)[926], col="green", lty="dashed")
 
+(ma9.ew926 <- arima(as.numeric(m.ibmvwewsp2603[1:926, 'EW']),
+                    order=c(0,0,9)))
+# The numbers don't match perfectly, but they are moderately close.
+# The difference may be due to difference in the accuracy of
+# the algorithm in finding the actual maximum of the likelihood
+# or to the likelihood surfact being fairly flat.
 
+(pred.ma9.ew936 <- predict(ma9.ew926, 10))
+# actual
+m.ibmvwewsp2603[927:936, "EW"]
 
+##
+## sec. 2.6.  Simple ARMA Models
+##
 
-
-
-
-
-
-
-# p. 21
-# Figure 1.6.  Time plot of daily exchange rate
-#              between US dollara and Japanese Yen 
+# pp. 59-60
+data(m.3m4697)
+str(m.3m4697)
+quantile(m.3m4697)
 
 op <- par(mfrow=c(2,1))
-plot(d.fxjp00, xlab="year", ylab="Yens", type="l")
-plot(diff(d.fxjp00), xlab="year", ylab="Change", type="l")
+plot(log(1+m.3m4697), xlab="year", ylab="l-rtn")
+
+acf(as.numeric(m.3m4697), main="")
 par(op)
 
-# p. 22
-# Table 1.3.  Descriptive Statistics of Selected US Financial Time Series
+# What to do about EACF?
+# Question sent to Prof. Tsay 2007.12.01
 
-(m.bondRtns <- rbind(
-  "1-12 months"=FinTS.stats(100*m.fama.bond5203[, "m1.12"]), 
-  "24-36 months"=FinTS.stats(100*m.fama.bond5203[, "m24.36"]), 
-  "48-60 months"=FinTS.stats(100*m.fama.bond5203[, "m48.60"]), 
-  "61-120 months"=FinTS.stats(100*m.fama.bond5203[, "m61.120"]) ))
 
-(m.treasuryRtns <- rbind(
-  "1 year"=FinTS.stats(m.gs1),
-  "3 years"=FinTS.stats(m.gs3),
-  "5 years"=FinTS.stats(m.gs5),
-  "10 years"=FinTS.stats(m.gs10) ))
 
-(w.treasuryRtns <- rbind(
-  "3 months"=FinTS.stats(w.tb3ms),
-  "6 months"=FinTS.stats(w.tb6ms) ) )
 
-#write.table(rbind(m.bondRtns, m.treasuryRtns, w.treasuryRtns),
-#            "Table1-3.csv", sep=",")
+# p. 64 
+##
+## sec. 2.7.  Unit-Root Nonstationarity
+##
+
+# p. 66
+data(m.3m4697)
+(mean.log.3m <- mean(log(1+m.3m4697)))
+sd(log(1+m.3m4697))
+
+# Figure 2.10.  Log prices for 3M stock
+
+log.3m <- cumsum(log(1+m.3m4697))
+log.3m0 <- cumsum(log(1+m.3m4697)-mean.log.3m)
+
+plot(log.3m, ylim=range(log.3m, log.3m0),
+     xlab="year", ylab="log-price")
+lines(log.3m0, lty="dotted", col="red")
+lines(t4697, mean.log.3m*(1:length(m.3m4697)))
+
+# p. 69
+# Example 2.2.  US quarterly GDP
+data(q.gdp4703)
+
+# p. 70
+# Figure 2.11.  
+op <- par(mfcol=c(2,2))
+plot(log(q.gdp4703), xlab="year", ylab="ln(GDP)",
+     main="(a)")
+
+acf(log(q.gdp4703), lag.max=16, main="(b)",
+    xlab="lag (years)")
+
+#plot(diff(q.gdp4703), xlab="year", ylab="diff(GDP)", main="(c)")
+# This is clearly NOT Figure 2.20.  
+plot(diff(log(q.gdp4703)), xlab="year", ylab="diff(ln(GDP))",
+     main="(c)")
+# Looks like Figure 2.10(c)
+
+pacf(diff(log(q.gdp4703)), lag.max=16, main="(d)",
+     xlab="lag (years)")
+par(op)
+
+qqnorm(diff(log(q.gdp4703)), datax=TRUE)
+abline(h=c(-2, 2))
+abline(h=-1.9)
+pnorm(-1.9)
+# 0.029 
+# Slightly nonnormal, with just over 5%
+# of observations = mild outliers.  
+# Therefore, I don't theink we need to worry about
+# nonnormality.
+
+library(tseries)
+adf.test(log(as.numeric(q.gdp4703)), a="e", k=9)
+#Dickey-Fuller = -0.9741, Lag order = 10, p-value = 0.05918
+
+library(urca)
+(fadf.c.l.gdp <- ur.df(log(q.gdp4703), type="drift", lags=9))
+summary(fadf.c.l.gdp)
+#Value of test-statistic is: -1.1306 7.0185 
+#Critical values for test statistics: 
+#      1pct  5pct 10pct
+#tau2 -3.46 -2.88 -2.57
+#phi1  6.52  4.63  3.81
+
+library(fUnitRoots)
+(fadf.c.l.gdp <- adfTest(log(q.gdp4703), type="c", lags=9))
+# Dickey-Fuller: -1.1306;  P VALUE: 0.6358 
+str(fadf.c.l.gdp)
+ 
+library(uroot)
+gdp.uroot <- ts(log(q.gdp4703), frequency=4, start=c(1947,1))
+
+ADF.d.l.gdp <- ADF.test(gdp.uroot, c(1,0,0),
+                        selectlags=list(mode=as.numeric(1:9)))
+#Warning message:
+#In interpolpval(code = code, stat = adfreg[, 3], N = N) :
+#  p-value is greater than printed p-value
+summary(ADF.d.l.gdp)
+summary(ADF.d.l.gdp@lmadf)
+# This call to ADF.test produces essentially the same answer
+# as S-Plus unitroot 
+
+# p. 71
+# Figure 2.12.  log(S&P 500), 1990.01.02 - 2003.12.31
+
+data(d.sp9003lev)
+plot(d.sp9003lev, xlab="year", ylab="S&P 500")
+plot(log(d.sp9003lev), xlab="year", ylab="ln(index)")
+
+fit.d.sp <- ar(as.numeric(diff(log(d.sp9003lev))))
+#Call: ar(x = as.numeric(diff(log(d.sp9003lev))))
+#Coefficients:
+#      1        2        3        4        5        6        7        8  
+#-0.0035  -0.0246  -0.0349   0.0046  -0.0369  -0.0191  -0.0450   0.0010  
+#      9       10       11       12       13  
+# 0.0094   0.0145  -0.0143   0.0512   0.0316  
+#Order selected 13  sigma^2 estimated as  0.0001101 
+
+str(fit.d.sp)
+aic.d.sp <- fit.d.sp$aic
+plot(names(aic.d.sp), aic.d.sp, type="b")
+
+# Order 13 = min AIC
+
+library(tseries)
+adf.test(as.numeric(log(d.sp9003lev)), k=14)
+# Dickey-Fuller = -0.9566, Lag order = 14, p-value = 0.9458
+# book:  -0.99658, p-value = 0.9469
+
+library(urca)
+(urca.c.l.sp <- ur.df(log(d.sp9003lev), type="trend", lags=14))
+summary(urca.c.l.sp)
+#Value of test-statistic is: -2.2389 3.7382 2.5972 
+#Critical values for test statistics: 
+#      1pct  5pct 10pct
+#tau3 -4.04 -3.45 -3.15
+#phi2  6.50  4.88  4.16
+#phi3  8.73  6.49  5.47
+
+library(fUnitRoots)
+(fadf.c.l.gdp <- adfTest(log(d.sp9003lev), lags=14, type="ct"))
+# Dickey-Fuller: -0.9566, P VALUE: 0.9458 
+# matches adf.test NOT ur.df
+# For q.gdp4703, adfTest matched uf.df NOT adf.test
+
+library(uroot)
+
+ADF.test(log(d.sp9003lev), c(1,1,0),
+         selectlags=list(mode="signf", Pmax=14))
+#Error in if (t0[2] != 1) { : missing value where TRUE/FALSE needed
+# ADF.test requires "a univariate time series object"
+# Here t0 = start(log(d.sp9003lev)),
+# which is a 'Date' object, NOT a vector of length 2.  
+str(d.sp9003lev)
+index(d.sp9003lev)[1:11]
+table(diff(index(d.sp9003lev)))
+#   1    2    3    4    7 
+#2766   35  648   81    1
+# Conclusion:
+# I'm not eager to try harder to use ADF.test
+
+# p. 72
+##
+## sec. 2.8.  Seasonal Models
+##
+data(q.jnj)
+
+op <- par(mfcol=c(2,1))
+plot(q.jnj, xlab="year", ylab="earnings",
+     main="(a) Earnings per share", type="o") 
+plot(log(q.jnj), xlab="year", ylab="ln-earnings",
+     main="(b) Log earnings per share", type="o")
+par(op)
+
+# p. 74
+# Figure 2.14.  
+op <- par(mfcol=c(2,2))
+acf(log(q.jnj))
+acf(diff(log(q.jnj)))
+acf(diff(log(q.jnj), 4))
+
+dxds <- diff(diff(log(q.jnj)), 4)
+dsdx <- diff(diff(log(q.jnj), 4))
+all.equal(dxds, dsdx)
+# TRUE 
+acf(dxds)
+par(op)
+
+# p. 76
+# Example 2.3
+
+(fit.jnj <- arima(log(q.jnj), order=c(0, 1, 1),
+                 seasonal=list(order=c(0, 1, 1))))
+str(fit.jnj)
+sqrt(fit.jnj$sigma2)
+
+# Figure 2.15
+# p. 77
+fit.jnj76 <- arima(log(q.jnj[1:76]), order=c(0, 1, 1),
+                 seasonal=list(order=c(0, 1, 1)))
+(pred.jnj76 <- predict(fit.jnj76, 8))
+
+str(q.jnj)
+plot(q.jnj[71:84], xlab="year", ylim=c(0, 30),
+     ylab="earning")
+points(exp(pred.jnj76$pred))
+ul <- with(pred.jnj76, pred+1.96*se)
+ll <- with(pred.jnj76, pred-1.96*se)
+
+lines(exp(ul), col="red", lty="dashed")
+lines(exp(ll), col="red", lty="dashed")
