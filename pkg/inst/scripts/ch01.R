47d4f4a49243bdbb5e0b1cfe3042c08b4cf7a1f5
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-26 21:51:56 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fints@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/scripts/ch01.R
+++ pkg/inst/scripts/ch01.R
@@ -17,14 +17,14 @@ library(FinTS)
 
 # p. 4
 # Table 1.1.  Illustration of the effects of compounding
-frequencies <- c(1, 2, 4, 12, 52, 365, Inf)
+freqs <- c(1, 2, 4, 12, 52, 365, Inf)
 (Table1.1 <- data.frame(Type=c(
     "Annual", "Semiannual", "Quarterly",
     "Monthly", "Weekly", "Daily", "Continuously"),
-  Number.of.payments=frequencies, 
-  Interest.rate.per.period=0.1/frequencies,
+  Number.of.payments=freqs, 
+  Interest.rate.per.period=0.1/freqs,
   Net.Value=compoundInterest(0.1,
-    frequency=frequencies) ) )
+    frequency=freqs) ) )
 
 # p.  6  
 # Example 1.1.
