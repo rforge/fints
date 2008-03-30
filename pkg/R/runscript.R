c921b601a005fd8f2294010f009c15006483df12
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-03-30 17:17:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fints@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/runscript.R
+++ pkg/R/runscript.R
@@ -1,5 +1,10 @@
-runscript <- function(x, fmt="ch%02d.R", package="FinTS", 
-                      subdir="scripts", lib.loc=NULL){ 
+runscript <- function(x, method=c('run', 'copy', 'view', 'dir'),  
+                      ask = TRUE, fmt="ch%02d.R", package="FinTS", 
+                      subdir="scripts", lib.loc=NULL){
+##
+## 1.  Set up
+##  
+  mtd <- match.arg(method) 
     # chnames <- c(...) # chapter names
     # if (missing(x)) x <- match(select.list(chnames), chnames)
     x <- as.numeric(x)
@@ -8,5 +13,33 @@ runscript <- function(x, fmt="ch%02d.R", package="FinTS",
 #    ch <- sprintf("ch%02d.R", x)
     ch <- sprintf(fmt, x)
     Ch <- paste(s, ch, sep="/")
+##
+## 2.  method == 'dir'
+##
+  if(method=='dir')return(Ch) 
+##
+## 3.  method == 'run'
+##
+  if(method=='run'){
+    if(ask){
+      op <- par(ask=TRUE) 
+      on.exit(par(op))
+    } 
     source(Ch, echo = TRUE)
+    return(invisible(Ch)) 
+  }
+##
+## 4.  method == 'view'
+##
+  ch. <- readLines(Ch)
+#  
+  if(method=='view'){
+    cat(ch., sep="\n") 
+    return(invisible(Ch)) 
+  }
+##
+## 5.  method == 'copy'
+##
+  writeLines(ch., ch)
+  return(invisible(Ch)) 
 }
