a23d26e1456984454d591e50ca29425d916ac191
spencerg <spencerg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-07-29 23:18:16 +0000

    fix problems of age
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fints@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FinTS/R/url2data.R
+++ pkg/FinTS/R/url2data.R
@@ -4,9 +4,11 @@ url2data <- function(url.){
   n <- length(fili)
   found <- rep(NA, n)
   if(n>0)for(i in 1:n){
-    chk <- try(download.file(url.[i], fili[i]))
+    chk <- try(utils::download.file(url.[i],
+                              fili[i]))
     found[i] <- (c("TRUE", "FALSE")[1 +
                (class(chk)=="try-error")])
   }
-  cbind(data=dati, file=fili, url=url., found=found) 
+  cbind(data=dati, file=fili, url=url., 
+        found=found) 
 }
